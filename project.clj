69abdd1add3ba06ebcb47592139a02eae6b5207d
zehcnas34 <zehcnas34@violet.city>
2023-05-07 20:49:11 -0700

    add deps

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "[FORK] A Clojure interface to the Lucene search engine"
   :url "http://github/city-violet/clucy"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.apache.lucene/lucene-core "9.1.0"]
-                 [org.apache.lucene/lucene-queryparser "9.1.0"]
-                 [org.apache.lucene/lucene-analyzers-common "8.11.1"]
-                 [org.apache.lucene/lucene-highlighter "9.1.0"]]
+                 [org.apache.lucene/lucene-core "4.2.0"]
+                 [org.apache.lucene/lucene-queryparser "4.2.0"]
+                 [org.apache.lucene/lucene-analyzers-common "4.2.0"]
+                 [org.apache.lucene/lucene-highlighter "4.2.0"]]
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}}
-  :codox {:src-dir-uri "http://github/city-violet/clucy/blob/master"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
+  :codox {:src-dir-uri               "http://github/city-violet/clucy/blob/master"
           :src-linenum-anchor-prefix "L"})
